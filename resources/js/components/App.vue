15672a85e9bc01db50ffff18bd4b063b9c0c25a4
Fernweh99 <daniele.dimento@gmail.com>
2022-10-04 14:47:27 +0200

    set and checked api and init vue

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,11 +1,27 @@
 <template>
   <div class="content">
-    <div class="title m-b-md">Coming Soon...</div>
+    <AppHeader/>
+    <main>
+      <IndexPosts/>
+    </main>
   </div>
 </template>
 
 <script>
+import IndexPosts from './posts/IndexPosts';
+import AppHeader from './AppHeader';
+
   export default {
     name: "App",
-  };
+    components: {
+    IndexPosts,
+    AppHeader
+},
+};
 </script>
+
+<style lang="scss">
+  main {
+    padding: 50px 0;
+  }
+</style>
---
+++ resources/js/components/posts/IndexPosts.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="posts">
+    <div class="container">
+      <h5>Posts:</h5>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPosts',
+};
+</script>
+
+<style>
+
+</style>
